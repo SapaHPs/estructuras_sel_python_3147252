b0c5fd5bae519324826d30550c3e8874c7508fe6
alyson <alysonpratosena24@gmail.com>
2025-03-11 10:20:40 +1200

    jerarquía

--- operadores_logicos.py
+++ operadores_logicos.py
@@ -7,17 +7,38 @@ Operadores logicos
 
 '''
 
-#Ejemplo 1: Operador not: 
+# Ejemplo 1: Operador not: 
 
-x = not True
-print("El resultado del oprerar con not es: ", x) 
+w = not True
+print("El resultado de oprerar con 'not' es: ", w) 
 
-#Ejemplo 2: Operador and: 
+# Ejemplo 2: Operador and: 
 
-y = True and True
-print("El resultado del oprerar con and es: ", y)
+x = True and True
+print("El resultado de oprerar con 'and' es: ", x)
 
-#Ejemplo 3: Operador or:
-z = False or False 
-print("El resultado del oprerar con or es: ", z)
+# Ejemplo 3: Operador or:
+y = False or False 
+print("El resultado de oprerar con 'or' es: ", y)
 
+'''
+
+Jerarquía de presedencia de los operadores 
+(logicos inclusive):
+
+    1.          ()
+    2.          **
+    3.          *, /, %,
+    4.          +, -
+    5.          <, >, <=, =>, !=, ==
+    6.          not
+    7.          and
+    8.          or
+    9.          =
+
+'''
+
+# Ejemplo 4: Jerarquía de operadores:
+
+z = False and not True or False
+print("El resultado de operar con Jerarquía de operadores es: ", z)  
